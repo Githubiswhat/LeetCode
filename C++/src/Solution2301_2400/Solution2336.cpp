4bb818172a87e6f1b76a66bfc99f2edc1386336e
Githubiswhat <43241621+Githubiswhat@users.noreply.github.com>
2023-11-29 12:29:34 +0800

    做题记录

--- C++/src/Solution2301_2400/Solution2336.cpp
+++ C++/src/Solution2301_2400/Solution2336.cpp
@@ -3,3 +3,44 @@
 //
 
 #include "Solution2336.h"
+#include <algorithm>
+#include <deque>
+using namespace std;
+
+class SmallestInfiniteSet {
+private:
+  int val = 1;
+  deque<int> list;
+
+
+public:
+  SmallestInfiniteSet() {
+
+  }
+
+  int popSmallest() {
+    if (!list.empty()){
+      int result = list.front();
+      list.pop_front();
+      return result;
+    }
+    return val++;
+  }
+
+  void addBack(int num) {
+    if (num < 1){
+      return;
+    }
+    if (num < val){
+      bool flag = false;
+      for (int i = 0; i < list.size(); ++i)
+        if (list[i] == num){
+          flag = true;
+      }
+      if (!flag){
+        list.push_back(num);
+        sort(list.begin(), list.end());
+      }
+    }
+  }
+};
