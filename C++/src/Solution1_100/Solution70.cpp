5b2b930709c5706320b56db49cf826a0a58aa304
Githubiswhat <43241621+Githubiswhat@users.noreply.github.com>
2023-12-23 16:20:47 +0800

    同步记录

--- C++/src/Solution1_100/Solution70.cpp
+++ C++/src/Solution1_100/Solution70.cpp
@@ -9,3 +9,13 @@
 #include <unordered_map>
 #include <string>
 using namespace std;
+
+int climbStairs(int n) {
+    int dp[n + 1];
+    dp[1] = 1;
+    dp[0] = 1;
+    for (int i = 2; i <= n; ++i) {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+    return dp[n];
+}
